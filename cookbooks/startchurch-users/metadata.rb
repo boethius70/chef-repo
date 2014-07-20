96bbc59e21274351fc98e36a596ae9a858221857
jantoniou@gmail.com <jantoniou@gmail.com>
2014-07-20 00:33:29 -0400

    deleted:    .chef/chef_server_backup/data_bags/mddxusers/dgebow.json deleted:    .chef/chef_server_backup/data_bags/mddxusers/jantoniou.json 	deleted:    .chef/chef_server_backup/data_bags/mddxusers/jlorance.json 	deleted:    .chef/chef_server_backup/data_bags/mddxusers/mddxadmin.json 	deleted:    .chef/chef_server_backup/data_bags/mddxusers/mddxdev.json 	deleted:    .chef/chef_server_backup/data_bags/mddxusers/nikola.json 	deleted:    .chef/chef_server_backup/data_bags/mddxusers/root.json 	modified:   cookbooks/startchurch-users/metadata.rb 	deleted:    cookbooks/startchurch-users/recipes/bash.rb 	deleted:    cookbooks/startchurch-users/recipes/default-back.rb 	modified:   cookbooks/startchurch-users/recipes/default.rb deleted:    cookbooks/startchurch-users/templates/default/mddx-databag.erb 	deleted:    cookbooks/syslog-ng

--- cookbooks/startchurch-users/metadata.rb
+++ cookbooks/startchurch-users/metadata.rb
@@ -1,7 +1,8 @@
-name             'mddx-users'
-maintainer       'MDDX,Inc.'
+name             'startchurch-users'
+maintainer       'Startchurch,Inc.'
 maintainer_email 'jantoniou@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures mddx-users'
+description      'Installs/Configures startchurch-users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+depends		 'user'
