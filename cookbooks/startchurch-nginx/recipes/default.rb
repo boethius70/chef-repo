b013de9e2fa75520b093bfbfad943e9a35fd5daf
jantoniou@gmail.com <jantoniou@gmail.com>
2014-07-21 01:03:29 -0400

    new file:   cookbooks/startchurch-nginx/CHANGELOG.md 	new file:   cookbooks/startchurch-nginx/README.md 	new file:   cookbooks/startchurch-nginx/metadata.rb 	new file:   cookbooks/startchurch-nginx/recipes/default.rb

---
+++ cookbooks/startchurch-nginx/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: startchurch-nginx
+# Recipe:: default
+#
+# Copyright 2014, Startchurch, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+php_fpm 'nginx' do
+  action :add
+  user 'nginx'
+  group 'nginx'
+  ip_address 127.0.0.1
+  port 9000
+  socket true
+  socket_path '/tmp/phpmyadmin.sock'
+  socket_perms "0666"
+  start_servers 2
+  min_spare_servers 2
+  max_spare_servers 8
+  max_children 8
+  terminate_timeout (node['php']['ini_settings']['max_execution_time'].to_i + 20)
+  value_overrides({
+    :error_log => "#{node['php']['fpm_log_dir']}/nginx-php.log"
+  })
+end
