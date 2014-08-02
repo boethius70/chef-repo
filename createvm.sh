a610b3d434922899feffb8ab7d0dfb0d160f1ca2
jantoniou@gmail.com <jantoniou@gmail.com>
2014-08-02 02:56:10 -0400

    deleted:    createvm-nonchef.sh 	modified:   createvm.sh

--- createvm.sh
+++ createvm.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
+
+error_msg() {
+	echo "Enter a hostname option first then the role type; e.g.,                   "
+        echo "./createvm.sh hostname sql|app                                            "
+        echo ""
+        echo "... where sql = Percona XtraDB Cluster Server                             "
+        echo "... and app = Startchurch Nginx+PHP web app server                        "
+}
+
 if [ "$1" != "" ];
 then
-	knife digital_ocean droplet create --server-name $1.startchurch.com --image 562354 --location 1 --size 66  --ssh-keys 123339,212918,36446 --bootstrap --run-list "role[startchurch-web-server]"
+	if [ "$2" != "" ];
+	then
+		case "$2" in
+			app)
+			ROLE="startchurch-web-server"
+			;;
+			sql)
+			ROLE="startchurch-percona"
+			;;	
+		esac
+		knife digital_ocean droplet create --server-name $1.startchurch.com --image 562354 --location 1 --size 66  --ssh-keys 123339,212918,36446 --bootstrap --run-list "role[$ROLE]"
+	else 
+		error_msg
+	fi
 else
-	echo "Enter a hostname option first; e.g.,  					"
-	echo "./createvm.sh hostname 							"
+ 	error_msg
 fi
