b69ae01748d638aeaf9c7fa2d4946442768a458f
jantoniou@gmail.com <jantoniou@gmail.com>
2014-08-02 14:10:41 -0400

    modified:   cookbooks/startchurch-percona/metadata.rb

--- cookbooks/startchurch-percona/metadata.rb
+++ cookbooks/startchurch-percona/metadata.rb
@@ -1,7 +1,9 @@
 name             'startchurch-percona'
 maintainer       'Startchurch, Inc'
-maintainer_email 'jantoniou@gmail.com"
+maintainer_email 'jantoniou@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures startchurch-percona'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+
+depends           'percona'
