fbe57cbb58c4dd1278daf067a8303de9362cbaf3
jantoniou@gmail.com <jantoniou@gmail.com>
2014-08-01 22:56:50 -0400

    modified:   createvm.sh

--- createvm.sh
+++ createvm.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-knife digital_ocean droplet create --server-name testjim.startchurch.com --image 562354 --location 1 --size 66  --ssh-keys 123339,212918,36446 --bootstrap --run-list "role[startchurch-web-server]" --ssh-port 22 --identity-file "/home/jantoniou/.ssh/id_rsa"
+if [ "$1" != "" ];
+then
+	knife digital_ocean droplet create --server-name $1.startchurch.com --image 562354 --location 1 --size 66  --ssh-keys 123339,212918,36446 --bootstrap --run-list "role[startchurch-web-server]"
+else
+	echo "Enter a hostname option first; e.g.,  					"
+	echo "./createvm.sh hostname 							"
+fi
