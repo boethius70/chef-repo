fd783d54c8d166cc77c1b8f0643d5a1f736e6e2c
jantoniou@gmail.com <jantoniou@gmail.com>
2014-08-06 04:51:49 -0400

    modified:   cookbooks/startchurch-percona/recipes/default.rb

--- cookbooks/startchurch-percona/recipes/default.rb
+++ cookbooks/startchurch-percona/recipes/default.rb
@@ -7,32 +7,12 @@
 # All rights reserved - Do Not Redistribute
 
 # Setup the Percona XtraDB Cluster
-cluster_ips = [192.34.60.135,104.131.198.79]
+cluster_ips = []
 unless Chef::Config[:solo]
   search(:node, 'role:percona').each do |other_node|
     next if other_node['private_ipaddress'] == node['private_ipaddress']
     Chef::Log.info "Found Percona XtraDB cluster peer: #{other_node['private_ipaddress']}"
     cluster_ips << other_node['private_ipaddress']
-  end
-end
-
-cluster_ips.each do |ip|
-  firewall_rule "allow Percona group communication to peer #{ip}" do
-    source ip
-    port 4567
-    action :allow
-  end
-
-  firewall_rule "allow Percona state transfer to peer #{ip}" do
-    source ip
-    port 4444
-    action :allow
-  end
-
-  firewall_rule "allow Percona incremental state transfer to peer #{ip}" do
-    source ip
-    port 4568
-    action :allow
   end
 end
 
